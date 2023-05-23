40130e3c7dd4a81ea99462e5d325cf72160e16e7
UGO <ugobourgoinscarfo@gmail.com>
2023-05-23 20:25:33 +0200

    css

--- frontend/src/components/ArtisteCard.jsx
+++ frontend/src/components/ArtisteCard.jsx
@@ -3,7 +3,22 @@ import PropTypes from "prop-types";
 import ArtisteType from "../types/artist";
 
 function ArtisteCard({ artiste }) {
-  return <div>{artiste.titre}</div>;
+  return (
+    <div className="fullbody">
+      <div className="container">
+        <div className="header">
+          <h2>{artiste.titre}</h2>
+        </div>
+        <div className="imginfo">
+          <img src={artiste.image.src} alt={artiste.image.alt} />
+        </div>
+        <div className="info">
+          <input type="button" value="More Info" />
+        </div>
+      </div>
+      ;
+    </div>
+  );
 }
 
 ArtisteCard.propTypes = {
--- frontend/src/pages/Artistes.jsx
+++ frontend/src/pages/Artistes.jsx
@@ -6,7 +6,7 @@ function Artistes() {
   return (
     <section>
       <div className="row">
-        <ArtisteCard artiste={artistes[0]} />
+        <ArtisteCard artiste={artistes[9]} />
       </div>
     </section>
   );
